03a85b10c7c2323e35b87ed0355ab88e5e3336ff
Junior <junior@certinergie.be>
2021-10-30 21:18:46 +0200

    WIP : Create Account Command

---
+++ Plutus.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Plutus.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
