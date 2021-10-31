2a40233096d0c6e6daefcf3366f6dfe32f86a1d7
Junior <junior@certinergie.be>
2021-10-31 12:13:52 +0100

    CreateAccount endpoint

---
+++ Plutus.Api/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Plutus.Application.AccountHandlers.Commands;
+
+namespace Plutus.Api.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    [Route("/api/accounts")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AccountController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        
+        [HttpPost("", Name = "CreateAccount")]
+        public async Task<ActionResult> CreateAccountAsync([FromBody] CreateAccountCommand command)
+        {
+            var accountId = await _mediator.Send(command);
+            return Ok(accountId);
+        }
+    }
+}
--- Plutus.Api/Program.cs
+++ Plutus.Api/Program.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Plutus.Application;
 using Plutus.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Plutus.Api", Version = "v1" }); });
 
